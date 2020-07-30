4286ba012b355e596eab4f07fcd1d99479c9e6ab
oc777 <oc222ba@student.lnu.se>
2020-07-30 11:11:22 +0200

    updating project structure

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Header } from './components/header/Header';
 import { Counter } from './components/counter/Counter';
-import { Home } from './components/home/Home';
-import { AddBox } from './components/box/AddBox';
-import { ListDispatches } from './components/dispatch/ListDispatches';
+import { Home } from './pages/home/Home';
+import { AddBox } from './pages/box/AddBox';
+import { ListDispatches } from './pages/dispatch/ListDispatches';
 
 import './style/main.css';
 //import './style/App.css';
---
+++ client/src/store/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import dispatch from "./dispatchReducers";
+
+export default combineReducers({
+  dispatch
+});
