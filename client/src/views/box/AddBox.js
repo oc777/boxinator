01bb507d608d31c7cf3a7bf7e19908c89d2aff3b
oc777 <oc222ba@student.lnu.se>
2020-07-31 21:00:41 +0200

    adding Country dropdown

--- client/src/views/box/AddBox.js
+++ client/src/views/box/AddBox.js
@@ -5,19 +5,64 @@ import { bindActionCreators } from 'redux';
 import { fetchCountries } from '../../store/actions/countryActions'
 
 class AddBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {country: '1'};
+    
+        this.handleCountryChange = this.handleCountryChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleCountryChange(event) {
+        this.setState({country: event.target.value}, () => {
+            console.log('new state ' + this.state.country)
+        })
+    }
+
+    handleSubmit(event) {
+        console.log('Submitted:');
+        event.preventDefault();
+    }
+
     componentDidMount() {
         // fetch
         this.props.fetchCountries();
+        console.log("Mounted")
+        console.log(this.state)
     }
+
+
 
     render() {
         const data = this.props.data.countryReducer;
         console.log(data)
+
+        let list;
+        if (data.destination || []) {
+            list = data.destination.map(item => {
+                return (
+                    <option value={item.id}>{item.name}</option>
+                )
+            });
+        }
+
         return (
-            <div className='addbox'>
-                <h1>Add new Box</h1>
-                <p>Here be boxes... or dragons... or forms...</p>
-            </div>
+            <React.Fragment>
+                <div className='addbox'>
+                    <h1>Add new Box</h1>
+                    <p>Here be boxes... or dragons... or forms...</p>
+                </div>
+    
+                <form onSubmit={this.handleSubmit}>
+                <label>
+                    Country
+                    <select value={this.state.country} onChange={this.handleDestinationChange}>
+                        {list}
+                    </select>
+                </label>
+                <input type="submit" value="Submit" />
+                </form>
+            </React.Fragment>
         );
     }
 }
