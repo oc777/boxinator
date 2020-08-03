b17be480f3f1b5cf860001fe1137d6984224eadc
oc777 <oc222ba@student.lnu.se>
2020-08-03 10:46:25 +0200

    adding checks to db schema

--- db/sql.d/init.sql
+++ db/sql.d/init.sql
@@ -9,8 +9,8 @@ CREATE TABLE IF NOT EXISTS countries (
 -- create table boxes
 CREATE TABLE IF NOT EXISTS boxes (
     box_id INT AUTO_INCREMENT PRIMARY KEY,
-    receiver VARCHAR(255) NOT NULL,
-    weight DOUBLE NOT NULL,
+    receiver VARCHAR(255) NOT NULL CHECK (receiver <> ''),
+    weight DOUBLE NOT NULL CHECK (weight > 0),
     color VARCHAR(25) NOT NULL,
     destination INT NOT NULL,
     shipping_cost DOUBLE NOT NULL,
